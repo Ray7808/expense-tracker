cca3fc0a3072e139e874f54e4508a21c5e516067
Ray7808 <yuwang7808@gmail.com>
2023-05-01 19:38:53 +0800

    feat: add new and edit page

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/edit', (req, res) => {
+  res.render('edit')
+})
+
 app.listen(port, () => {
   console.log(`Now listening http://localhost:${port}`)
 })
