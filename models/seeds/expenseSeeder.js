0e7210729b48bd472542bd705168299a1948a4e3
Ray7808 <yuwang7808@gmail.com>
2023-05-03 18:53:33 +0800

    feat: add expenseSeeder.js to create user information

--- models/seeds/expenseSeeder.js
+++ models/seeds/expenseSeeder.js
@@ -8,27 +8,35 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const SEED_USER_1 = {
-  name: 'user1',
-  email: 'user1@example.com',
-  password: '12345678',
-}
-const SEED_USER_2 = {
-  name: 'user2',
-  email: 'user2@example.com',
-  password: '12345678',
-}
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+  },
+]
 
 db.once('open', () => {
-  bcrypt
-    .genSalt(10)
-    .then((salt) => bcrypt.hash(SEED_USER_1.password, salt))
-    .then((hash) =>
-      userInfo.create({
-        name: SEED_USER_1.name,
-        email: SEED_USER_1.email,
-        password: hash,
-      })
-    )
-  console.log('done!')
+  Promise.all(
+    SEED_USER.map(async (user) => {
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(user.password, salt))
+        .then((hash) =>
+          userInfo.create({
+            name: user.name,
+            email: user.email,
+            password: hash,
+          })
+        )
+    })
+  ).then(() => {
+    console.log('done!')
+    process.exit()
+  })
 })
