2548eb04d592f94c24bfdeb90b54d025d572d5fa
Ray7808 <yuwang7808@gmail.com>
2023-05-04 21:32:29 +0800

    feat: add new.hbs and new router create the new data

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -6,6 +6,27 @@ const categoryInfo = require('../../models/category')
 router.get('/new', (req, res) => {
   res.render('new')
 })
+router.post('/new', (req, res) => {
+  const userId = req.user._id
+
+  const category = req.body.category
+
+  return categoryInfo
+    .find({ name: `${category}` })
+    .lean()
+    .then((categoryContent) => {
+      const categoryId = categoryContent[0]._id
+      return expenseInfo.create({
+        name: `${req.body.name}`,
+        date: `${req.body.date}`,
+        amount: `${req.body.amount}`,
+        userId,
+        categoryId,
+      })
+    })
+    .then(() => res.redirect('/'))
+    .catch((err) => console.log(err))
+})
 
 router.get('/:id/edit', (req, res) => {
   const _id = req.params.id
@@ -18,11 +39,8 @@ router.get('/:id/edit', (req, res) => {
     })
 })
 
-router.post('/new', (req, res) => {
-  res.redirect('/')
-})
 router.post('/:id/edit', (req, res) => {
-  //根據動態路由輸入的id，將編輯後的資料傳至mongoDB並重新導向到edit.hbs
+  //根據動態路由輸入的id，將編輯後的資料傳至mongoDB並重新導向到index.hbs
   const _id = req.params.id
   const userId = req.user._id
 
