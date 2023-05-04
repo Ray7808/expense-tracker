965979071c9061d30eda87f479a33106ffc0507f
Ray7808 <yuwang7808@gmail.com>
2023-05-04 21:11:24 +0800

    feat: add post /expense/edit to edit the expense information

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const expenseInfo = require('../../models/expense')
+const categoryInfo = require('../../models/category')
+
+router.get('/new', (req, res) => {
+  res.render('new')
+})
 
 router.get('/:id/edit', (req, res) => {
   const _id = req.params.id
@@ -9,9 +14,31 @@ router.get('/:id/edit', (req, res) => {
     .findOne({ _id, userId })
     .lean()
     .then((expense) => {
-      console.log(expense.date)
       res.render('edit', { expense, _id })
     })
+})
+
+router.post('/new', (req, res) => {
+  res.redirect('/')
+})
+router.post('/:id/edit', (req, res) => {
+  //根據動態路由輸入的id，將編輯後的資料傳至mongoDB並重新導向到edit.hbs
+  const _id = req.params.id
+  const userId = req.user._id
+
+  return categoryInfo
+    .findOne({ name: req.body.category })
+    .then((category) => {
+      return expenseInfo.findOne({ _id, userId }).then((expense) => {
+        expense.name = req.body.name
+        expense.date = req.body.date
+        expense.amount = req.body.amount
+        expense.categoryId = category._id
+        return expense.save()
+      })
+    })
+    .then(() => res.redirect(`/`))
+    .catch((error) => console.log(error))
 })
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -35,20 +35,4 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/new', (req, res) => {
-  res.render('new')
-})
-
-router.get('/edit', (req, res) => {
-  res.render('edit')
-})
-router.post('/new', (req, res) => {
-  console.log(req.body)
-  res.redirect('/')
-})
-router.post('/edit', (req, res) => {
-  console.log(req.body)
-  res.redirect('/')
-})
-
 module.exports = router
